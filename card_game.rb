b79c35caaa843a0f04cc40106a4ef9d00831a433
Daniel <daniel.directNOSPAM@gmail.com>
2011-09-12 18:33:19 +0800

    add #deal method

--- card_game.rb
+++ card_game.rb
@@ -1,15 +1,32 @@
+require 'ruby-debug'
+
 class CardGame
-  attr_accessor :cards
+  attr_accessor :cards, :players
 
   def initialize(decks=1)
-    self.cards = []
+    @cards = []
+    @players = []
 
     decks.times do
       %w(A 2 3 4 5 6 7 8 9 T J Q K).each do |rank|
         %w(c s d h).each do |suit|
-          cards << rank + suit
+          @cards << rank + suit
         end
       end
     end
+  end
+
+  def deal
+    player_cards = []
+    5.times { player_cards << @cards.shuffle!.pop }
+    @players << Player.new(player_cards)
+  end
+end
+
+class Player
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
   end
 end
--- card_game_spec.rb
+++ card_game_spec.rb
@@ -1,8 +1,10 @@
 require './card_game'
 
 describe CardGame do
+  let(:game1) { CardGame.new }
+  let(:game2) { CardGame.new(2) }
+
   describe "#new" do
-    let(:game1) { CardGame.new }
 
     it 'is valid' do
       game1.should be
@@ -18,10 +20,36 @@ describe CardGame do
     end
 
     context 'with optional parameter of 2' do
-      let(:game2) { CardGame.new(2) }
       it 'creates 2 decks of cards' do
         game2.cards.count.should == 52 * 2
       end
+    end
+  end
+
+  describe "#deal" do
+    before(:all) do
+      game1.deal
+    end
+
+    it 'involves 1 player' do
+      game1.players.count.should == 1
+    end
+
+    it 'lowers count of cards' do
+      game1.cards.count.should == 52 - 5
+    end
+
+    it 'removes played cards from deck' do
+      player_cards = game1.players.first.cards
+      game1.cards.should_not include(player_cards)
+    end
+
+    it 'assigns cards to player' do
+      game1.players.first.class.should == Player
+    end
+
+    it 'assigns cards randomly' do
+      pending "dunno how to test this"
     end
   end
 end
