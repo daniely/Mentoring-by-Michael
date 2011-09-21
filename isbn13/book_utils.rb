759bb10fd5a86f455d69dca430b5dd86d9ba00a7
Daniel <robotright@gmail.com>
2011-09-21 13:06:22 +0800

    psuedo code

--- isbn13/book_utils.rb
+++ isbn13/book_utils.rb
@@ -5,4 +5,10 @@ class BookUtils
     last_isbn = isbn.split('')[-1].to_i
     last_isbn == check_digit(isbn)
   end
+
+  def self.check_digit(isbn)
+    # alternate n + n*3
+    # result % 10
+    # 10 - result
+  end
 end
