cd5c670f0b291e84ab2316a47e592b0b6b440653
Daniel <robotright@gmail.com>
2011-09-21 13:37:25 +0800

    tiny bit more readable

--- isbn13/book_utils.rb
+++ isbn13/book_utils.rb
@@ -2,7 +2,7 @@ class BookUtils
   # algo
   # last isbn digit == check digit?
   def self.valid_isbn13?(isbn)
-    last_isbn = isbn.split('')[-1].to_i
+    last_isbn = isbn.split('').last.to_i
     last_isbn == check_digit(isbn)
   end
 
