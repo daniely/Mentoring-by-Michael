a2f09af7f73962efbc7a0a7c79784c0fc3b6db5c
Daniel <robotright@gmail.com>
2011-09-21 13:22:14 +0800

    algo filled in but not working. must break down

--- isbn13/book_utils.rb
+++ isbn13/book_utils.rb
@@ -7,8 +7,23 @@ class BookUtils
   end
 
   def self.check_digit(isbn)
+    # remove non-digits
+    isbn.gsub!('-','')
+
+    # get it in a format we can work with
+    isbn_a = isbn.split('').map{ |n| n.to_i }
+
+    # ignore last digit
+    isbn_a = isbn_a[0..-2]
+
     # alternate n + n*3
+    alt = isbn_a.each_with_index \
+                .inject(0){ |sum, (n,i)| sum += i.next % 2 ? n : n*3 }
+
     # result % 10
+    alt = alt % 10
+
     # 10 - result
+    alt = 10 - alt
   end
 end
