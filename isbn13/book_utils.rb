8f090b6e10dd3e1dc919039a66b925ad88f91eef
Daniel <robotright@gmail.com>
2011-09-21 13:04:18 +0800

    check if last isbn digit matches check digit

--- isbn13/book_utils.rb
+++ isbn13/book_utils.rb
@@ -1,5 +1,8 @@
 class BookUtils
+  # algo
+  # last isbn digit == check digit?
   def self.valid_isbn13?(isbn)
-    true
+    last_isbn = isbn.split('')[-1].to_i
+    last_isbn == check_digit(isbn)
   end
 end
