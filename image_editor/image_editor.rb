84ad7c4b0345caf6b5d25891b5c313742325fcf1
Daniel <robotright@gmail.com>
2011-09-27 23:35:20 +0800

    parse commands

--- image_editor/image_editor.rb
+++ image_editor/image_editor.rb
@@ -1,13 +1,23 @@
 require 'ruby-debug'
 
 class ImageEditor
+  DEFAULT_COLOR = 'O'
+
   attr_reader :image
 
   def initialize
     @image = []
   end
 
-  def execute(commands)
-    5.times { @image << [*1..6] }
+  def execute(input)
+    commands = input.split
+    command = commands.shift
+
+    case command
+    when "I"
+      width = commands.shift.to_i
+      height = commands.shift.to_i
+      @image = [DEFAULT_COLOR * width] * height
+    end
   end
 end
--- image_editor/image_editor_spec.rb
+++ image_editor/image_editor_spec.rb
@@ -2,13 +2,13 @@ require './image_editor'
 
 describe ImageEditor do
   describe "commands" do
+    let(:ie) { ImageEditor.new }
+
     describe "I 5 6" do
       it 'creates a new MxN image' do
-        ie = ImageEditor.new
         ie.execute("I 5 6")
-        # internal representation of image should be 5 x 6 and white
-        ie.image.size.should == 5
-        ie.image.flatten.size.should == 5 * 6
+        ie.image.first.size.should == 5
+        ie.image.size.should == 6
       end
     end
   end
