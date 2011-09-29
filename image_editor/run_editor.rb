90b88a782140c257397373e13f7e0374a7289a88
Daniel <robotright@gmail.com>
2011-09-29 10:24:03 +0800

    small refactor

--- image_editor/run_editor.rb
+++ image_editor/run_editor.rb
@@ -1,8 +1,7 @@
 require './image_editor'
 
 def run_editor
-  prompt = "Commands: I M N, C, L X Y C, V X Y1 Y2 C, H X1 X2 Y, F X Y C, S X"
-  puts prompt
+  puts "Commands: I M N, C, L X Y C, V X Y1 Y2 C, H X1 X2 Y, F X Y C, S X"
   ie = ImageEditor.new
 
   while (input = gets.chomp) != 'x'
