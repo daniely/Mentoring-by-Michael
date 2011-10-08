9b5c5273cdae8f73472d96158c2776079a0df469
Daniel <robotright@gmail.com>
2011-10-08 21:45:53 +0800

    works for single value

--- subarray_sum/biggest_subarray.rb
+++ subarray_sum/biggest_subarray.rb
@@ -3,9 +3,16 @@ describe "#biggest_subarray" do
     it 'returns [5, [5]]' do
       biggest_subarray([5]).should == [5, [5]]
     end
+
+    describe "biggest_subarry([4])" do
+      it 'returns [4, [4]]' do
+        biggest_subarray([4]).should == [4, [4]]
+      end
+    end
   end
 end
 
 def biggest_subarray(ar)
-  [5, [5]]
+  first = ar.pop
+  [first, [first]]
 end
