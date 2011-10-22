5d8e28dde853bd61ff1c8bfa8ced1f11bfdd384b
Daniel <robotright@gmail.com>
2011-10-22 19:25:52 +0800

    move variable out of spec

--- shift_subtitle/shift_subtitle_spec.rb
+++ shift_subtitle/shift_subtitle_spec.rb
@@ -37,17 +37,6 @@ describe ShiftSubtitle do
 
   describe "shift subs from text" do
     it 'outputs shifted subs' do
-      shifted =<<-EOS
-645
-01:31:53,710 --> 01:31:57,393
-the government is implementing a new policy...
-
-646
-01:31:57,428 --> 01:32:00,164
-In connection with a dramatic increase
-in crime in certain neighbourhoods,
-EOS
-
       amount = 2500
       ShiftSubtitle::shift_stream(sample_subs, amount).should == shifted
     end
