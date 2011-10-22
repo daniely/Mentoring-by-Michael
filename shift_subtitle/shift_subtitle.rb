37f86559d93afe7cac38bd6db239fc2c70909bb6
Daniel <robotright@gmail.com>
2011-10-22 18:50:55 +0800

    add shift_stream method

--- shift_subtitle/shift_subtitle.rb
+++ shift_subtitle/shift_subtitle.rb
@@ -2,6 +2,21 @@ require 'time'
 require 'ruby-debug'
 
 module ShiftSubtitle
+  def self.shift_stream(stream, amount)
+    result = ''
+
+    stream.each_line do |line|
+      if line.match(/\d{2}:\d{2}:\d{2},\d{2}/)
+        start, finish = line.split(' --> ')
+        result << "#{ShiftSubtitle::shift(start, amount)} --> #{ShiftSubtitle::shift(finish, amount)}\n"
+      else
+        result << line
+      end
+    end
+
+    result
+  end
+
   def self.shift(time, amount)
     parsed_time = Time.parse(time)
     amount_in_seconds = amount / 1000.0
--- shift_subtitle/shift_subtitle_spec.rb
+++ shift_subtitle/shift_subtitle_spec.rb
@@ -13,6 +13,47 @@ EOS
 
 # shift_subtitle --operation add --time 02,110 input_file output_file
 
+describe "shift subs from file" do
+  it 'creates shifted subs file' do
+    shifted =<<-EOS
+645
+01:31:53,710 --> 01:31:57,393
+the government is implementing a new policy...
+
+646
+01:31:57,428 --> 01:32:00,164
+In connection with a dramatic increase
+in crime in certain neighbourhoods,
+EOS
+
+    amount = 2500
+    File.open('subs.srt', 'r') do |f|
+      debugger
+      x = 3
+    end
+
+    ShiftSubtitle::shift_stream(sample_subs, amount).should == shifted
+  end
+end
+
+describe "shift subs from text" do
+  it 'outputs shifted subs' do
+    shifted =<<-EOS
+645
+01:31:53,710 --> 01:31:57,393
+the government is implementing a new policy...
+
+646
+01:31:57,428 --> 01:32:00,164
+In connection with a dramatic increase
+in crime in certain neighbourhoods,
+EOS
+
+    amount = 2500
+    ShiftSubtitle::shift_stream(sample_subs, amount).should == shifted
+  end
+end
+
 describe ShiftSubtitle do
   describe "#shift" do
     it 'shifts 01:31:51,210' do
