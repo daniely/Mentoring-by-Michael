2837c8435ee32d6bca2cc121cd945a1c5a3a7a16
Daniel <robotright@gmail.com>
2011-11-08 12:32:54 +0800

    remove rspec

--- memo_fib/memo_fib_hash.rb
+++ memo_fib/memo_fib_hash.rb
@@ -1,13 +1,7 @@
-require 'ruby-debug'
-
-describe "fib hash" do
-  it 'use block to create fib hash' do
-    fib = Hash.new do |h, k|
-      h[k] = h[k-1] + h[k-2]
-    end
-    fib.merge!({ 0 => 0, 1 => 1 })
-
-    puts "fib 6 is #{fib[6]}"
-    p fib
-  end
+fib = Hash.new do |h, k|
+  h[k] = h[k-1] + h[k-2]
 end
+fib.merge!({ 0 => 0, 1 => 1 })
+
+puts "fib 6 is #{fib[6]}"
+p fib
