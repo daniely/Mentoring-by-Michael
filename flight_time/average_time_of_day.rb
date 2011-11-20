5db4e201fbbf454c060351dc19777bc787b8b029
Daniel <robotright@gmail.com>
2011-11-21 04:49:14 +0800

    remove dependence on instance variable

--- flight_time/average_time_of_day.rb
+++ flight_time/average_time_of_day.rb
@@ -3,41 +3,38 @@ require 'time'
 
 DAY_IN_SECONDS = 24 * 60 * 60
 
-def average_time_of_day(times)
+def average_time_of_day(times, arrival_time="10pm")
   # sum diff of each from arrival_time
   # div by # of elements
   # add to arrival_time
 
-  meridian = @arrival_time.match(/[ap]m/)[0]
+  meridian = arrival_time.match(/[ap]m/)[0]
 
   avg_time_diff = times.map! do |t|
     i_time = Time.parse(t)
     i_time += DAY_IN_SECONDS unless t.match(meridian)
-    i_time - Time.parse(@arrival_time)
+    i_time - Time.parse(arrival_time)
   end.inject(:+) / times.size
 
-  (Time.parse(@arrival_time) + avg_time_diff).strftime('%l:%M%p').strip.downcase
+  (Time.parse(arrival_time) + avg_time_diff).strftime('%l:%M%p').strip.downcase
 end
 
 describe "average_time_of_day" do
+  let(:arrival_time) { "10pm" }
+
   it '12:01am' do
-    @arrival_time = "10pm"
     average_time_of_day(["11:51pm", "11:56pm", "12:01am", "12:06am", "12:11am"]).should == "12:01am"
   end
 
   it '6:51am' do
-    @arrival_time = "10pm"
     average_time_of_day(["6:41am", "6:51am", "7:01am"]).should == "6:51am"
   end
 
   it '10:33pm' do
-    @arrival_time = "10pm"
     average_time_of_day(["10:20pm", "10:30pm", "10:50pm"]).should == "10:33pm"
   end
 
   it '6:33am' do
-    @arrival_time = "10pm"
     average_time_of_day(["10:20am", "10:30am", "10:50pm"]).should == "6:33am"
   end
-
 end
