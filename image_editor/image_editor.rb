482ae297b6f4c26cf943480e9f7f5912bead9b64
Daniel <robotright@gmail.com>
2013-06-29 20:57:05 +0800

    refactor image editor in a more composable/OOP way

--- image_editor/image_editor.rb
+++ image_editor/image_editor.rb
@@ -1,12 +1,104 @@
-require 'ruby-debug'
+require 'pry'
+
+class Commands
+  attr_reader :commands
+
+  def initialize
+    @commands = {}
+    %w[i v h l f c s].each do |letter|
+      @commands[letter] = public_method("command_#{letter}")
+    end
+  end
+
+  def command_i(ie, input)
+    width = input.shift.to_i
+    height = input.shift.to_i
+
+    if width < 1 || height < 1 || width > 250 || height > 250
+      ie.output.puts "Image must be 1 <= m,n <= 250"
+      return
+    end
+
+    ie.image = []
+    height.times { ie.image << [ImageEditor::DEFAULT_COLOR] * width }
+  end
+
+  def command_v(ie, input)
+    x = input.shift.to_i
+    y1 = input.shift.to_i
+    y2 = input.shift.to_i
+    color = input.shift
+
+    (y1..y2).each { |y| ie.image[y-1][x-1] = color }
+  end
+
+  def command_h(ie, input)
+    x1 = input.shift.to_i
+    x2 = input.shift.to_i
+    y = input.shift.to_i
+    color = input.shift
+
+    (x1..x2).each { |x| ie.image[y-1][x-1] = color }
+  end
+
+  def command_l(ie, input)
+    x = input.shift.to_i
+    y = input.shift.to_i
+    color = input.shift
+
+    ie.image[y-1][x-1] = color
+  end
+
+  def command_f(ie, input)
+    x = input.shift.to_i
+    y = input.shift.to_i
+    new_color = input.shift
+    old_color = ie.image[y][x]
+
+    fill(ie, x, y, old_color, new_color)
+  end
+
+  def command_c(ie, input)
+    command_i(ie, [ie.image.first.size, ie.image.size])
+  end
+
+  def command_s(ie, input)
+    ie.output.puts ie.to_s
+  end
+
+  private
+
+  def fill(ie, x, y, old_color, new_color)
+    # exit if already colored
+    return if ie.image[y][x] == new_color
+
+    command_l(ie, [x.next, y.next, new_color])
+
+    # get valid adjacent pts
+    adjacent_coords = [x-1, x+1].product([y]) + [x].product([y-1, y+1])
+    adjacent_coords.delete_if{ |c| c.first < 0 || c.last < 0 }
+    adjacent_coords.delete_if{ |c| c.first > ie.image.first.size - 1 || c.last > ie.image.size - 1 }
+
+    # only fill if color is the same
+    adjacent_coords.delete_if{ |c| ie.image[c.last][c.first] != old_color }
+
+    adjacent_coords.each { |a| fill(ie, *a, old_color, new_color) }
+  end
+end
 
 class ImageEditor
   DEFAULT_COLOR = 'O'
 
-  attr_reader :image, :output
+  attr_accessor :image, :output, :commands
 
   def initialize(output=STDOUT)
     @output = output
+    @commands = Commands.new.commands
+    @image = []
+  end
+
+  def add_command(key, &block)
+    commands[key] = block
   end
 
   def to_s
@@ -16,68 +108,7 @@ class ImageEditor
   def execute(input)
     input = input.split
 
-    case input.shift
-    when "I"
-      width = input.shift.to_i
-      height = input.shift.to_i
-
-      if width < 1 || height < 1 || width > 250 || height > 250
-        @output.puts "Image must be 1 <= m,n <= 250" 
-        return
-      end
-
-      @image = []
-      height.times { @image << [DEFAULT_COLOR] * width }
-    when "V"
-      x = input.shift.to_i
-      y1 = input.shift.to_i
-      y2 = input.shift.to_i
-      color = input.shift
-
-      (y1..y2).each { |y| @image[y-1][x-1] = color }
-    when "H"
-      x1 = input.shift.to_i
-      x2 = input.shift.to_i
-      y = input.shift.to_i
-      color = input.shift
-
-      (x1..x2).each { |x| @image[y-1][x-1] = color }
-    when "L"
-      x = input.shift.to_i
-      y = input.shift.to_i
-      color = input.shift
-
-      @image[y-1][x-1] = color
-    when "F"
-      x = input.shift.to_i
-      y = input.shift.to_i
-      new_color = input.shift
-      old_color = @image[y][x]
-
-      fill(x, y, old_color, new_color)
-    when "C"
-      execute("I #{@image.first.size} #{@image.size}")
-    when "S"
-      @output.puts to_s
-    else
-      @output.puts "Bad command. Please try again."
-    end
-  end
-
-  def fill(x, y, old_color, new_color)
-    # exit if already colored
-    return if @image[y][x] == new_color
-
-    execute("L #{x.next} #{y.next} #{new_color}")
-
-    # get valid adjacent pts
-    adjacent_coords = [x-1, x+1].product([y]) + [x].product([y-1, y+1])
-    adjacent_coords.delete_if{ |c| c.first < 0 || c.last < 0 }
-    adjacent_coords.delete_if{ |c| c.first > @image.first.size - 1 || c.last > @image.size - 1 }
-
-    # only fill if color is the same
-    adjacent_coords.delete_if{ |c| @image[c.last][c.first] != old_color }
-
-    adjacent_coords.each { |a| fill(*a, old_color, new_color) }
+    command = input.shift
+    commands[command.downcase].call(self, input)
   end
 end
